530d3dc8c0b44312cceef72c4a7f4e9ad8f8a67c
Adrien Girbone <adrien@bunkerpalace.com>
2018-02-02 17:29:19 +0100

    First web client build

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'normalize.css';
-import './styles/index.scss';
-import App from './components/App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
